b4bc87cb01c847fe41561aaa0225235ba216764a
Adam Joyce <adamjoyce@outlook.com>
2017-06-22 18:45:02 +0100

    Added camera controller with smooth mouse control

---
+++ trails/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float lookSensitivity = 5;
+    private float yRotation;
+    private float xRotation;
+    private float currentYRotation;
+    private float currentXRotation;
+    private float yRotationV = 0.0f;
+    private float xRotationV = 0.0f;
+    public float lookSmoothDamp = 0.1f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
+
+        transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+    }
+}
