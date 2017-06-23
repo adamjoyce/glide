94c4724a535e9b55d6e3bc21d16a0a78dbf6cb4e
Adam Joyce <adamjoyce@outlook.com>
2017-06-23 16:50:26 +0100

    Meteor meshes now rotate

--- trails/Assets/Scripts/Meteor.cs
+++ trails/Assets/Scripts/Meteor.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Meteor : MonoBehaviour
 {
     public GameObject impactZone;           // The centre of the area the meteor will aim towards.
-    public float speed = 5.0f;              // The speed the meteor will travel at.
+    public float movementSpeed = 5.0f;      // The speed the meteor will travel at.
+    public float rotationSpeed = 90.0f;     // The speed at which the meteor rotates.
 
+    private Transform meteorMesh;          // The meteor's mesh that is visible in the scene.
     private Vector3 impactZoneSize;         // The dimensions of the box collider on the impact zone gameobject.
     private Vector3 targetImpactLocation;   // The target location the meteor will aim for.
 
@@ -14,6 +16,7 @@ public class Meteor : MonoBehaviour
     void Start()
     {
         impactZoneSize = impactZone.GetComponent<BoxCollider>().bounds.size;
+        meteorMesh = transform.GetChild(0);
 
         // Assumes that the impact zone as equal dimensions and is centred at the world origin.
         float halfImpactZoneSize = impactZoneSize.x * 0.5f;
@@ -25,6 +28,7 @@ public class Meteor : MonoBehaviour
     /* Update is called once per frame. */
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetImpactLocation, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetImpactLocation, movementSpeed * Time.deltaTime);
+        meteorMesh.RotateAround(meteorMesh.position, -Vector3.right, rotationSpeed * Time.deltaTime);
     }
 }
