f24f5d1b58fa8bf22f94c51c6cf8d173c357e9a4
Adam Joyce <adamjoyce@outlook.com>
2017-06-23 01:38:49 +0100

    Implemented the cannonballs launching from the camera

---
+++ trails/Assets/Scripts/Cannon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject cannonBall;           // The gameobject that will be fired when space is pressed.
+    public GameObject cannon;               // The gameobject the projectiles will originate from.
+    public float shootForce = 0.0f;         // The initial force applied to the cannon ball.
+
+    /* Update is called once per frame. */
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(cannonBall, cannon.transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
+        }
+    }
+}
