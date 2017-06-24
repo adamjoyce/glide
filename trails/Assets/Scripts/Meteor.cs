8e6c108fa3b2cfe33360bef4c5ed4830183e6ce2
Adam Joyce <adamjoyce@outlook.com>
2017-06-24 14:24:28 +0100

    Added fireball "Meteorite" prefab and basic collision detection between cannonball's and meteorites

--- trails/Assets/Scripts/Meteor.cs
+++ trails/Assets/Scripts/Meteor.cs
@@ -13,7 +13,7 @@ public class Meteor : MonoBehaviour
     private Vector3 targetImpactLocation;   // The target location the meteor will aim for.
 
     /* Use this for initialization. */
-    void Start()
+    private void Start()
     {
         impactZoneSize = impactZone.GetComponent<BoxCollider>().bounds.size;
         meteorMesh = transform.GetChild(0);
@@ -23,12 +23,23 @@ public class Meteor : MonoBehaviour
         targetImpactLocation = new Vector3(Random.Range(-halfImpactZoneSize, halfImpactZoneSize) + impactZone.transform.position.x,
                                            Random.Range(-halfImpactZoneSize, halfImpactZoneSize) + impactZone.transform.position.y,
                                            Random.Range(-halfImpactZoneSize, halfImpactZoneSize) + impactZone.transform.position.z);
+        transform.LookAt(targetImpactLocation);
     }
 
     /* Update is called once per frame. */
-    void Update()
+    private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetImpactLocation, movementSpeed * Time.deltaTime);
         meteorMesh.RotateAround(meteorMesh.position, -Vector3.right, rotationSpeed * Time.deltaTime);
+    }
+
+    /* What happens when something collides with this object. */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cannonball")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }
