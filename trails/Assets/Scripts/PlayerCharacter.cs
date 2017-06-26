209da499e00b916975f5d6b920108f087daceb3d
Adam Joyce <adamjoyce@outlook.com>
2017-06-26 18:41:23 +0100

    Refactored cannon firing into PlayerCharacter.cs in preparation for health inclusion

--- trails/Assets/Scripts/PlayerCharacter.cs
+++ trails/Assets/Scripts/PlayerCharacter.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public GameObject cannonBall;           // The gameobject that will be fired when space is pressed.
+    public GameObject cannon;               // The gameobject the projectiles will originate from.
+    public float shootForce = 0.0f;         // The initial force applied to the cannonball.
+    public float shotDelay = 1.0f;          // The time that needs to pass before another shot can be taken.
 
-    // Use this for initialization
+    private float nextFireTime = 0.0f;      // The next time increment at which another cannonball may be fired.
+
+    /* Use this for initialization. */
     void Start()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    /* Update is called once per frame. */
+    private void Update()
     {
+        if ((Time.time >= nextFireTime) && Input.GetKeyDown(KeyCode.Space))
+        {
+            FireCannonball();
 
+            // Update when the cannon can next fire.
+            nextFireTime = Time.time + shotDelay;
+        }
+    }
+
+    /* Spawns and fires a cannonball. */
+    private void FireCannonball()
+    {
+        GameObject projectile = Instantiate(cannonBall, cannon.transform.position, transform.rotation);
+        projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
     }
 }
