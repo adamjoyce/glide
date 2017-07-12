4c68447db29a6204bf466e3567a08e89ca4dd87b
Adam Joyce <adamjoyce@outlook.com>
2017-07-12 19:04:38 +0100

    WIP - Arrow indicators

--- trails/Assets/Scripts/MonoBehaviours/IndicatorController.cs
+++ trails/Assets/Scripts/MonoBehaviours/IndicatorController.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class IndicatorController : MonoBehaviour
 {
-    public MeteorManager meteorManager;                                 // The script that manages the active meteors.
-    public GameObject targetIndicatorPrefab;                                 // The prefab for the target UI GameObject element displayed when the object is on-screen.
-    public GameObject arrowIndicatorPrefab;                                  // The prefab for the arrow UI GameObject element displayed when the object is off-screen.
+    public MeteorManager meteorManager;                                          // The script that manages the active meteors.
+    public GameObject targetIndicatorPrefab;                                     // The prefab for the target UI GameObject element displayed when the object is on-screen.
+    public GameObject arrowIndicatorPrefab;                                      // The prefab for the arrow UI GameObject element displayed when the object is off-screen.
 
     [SerializeField]
-    private List<GameObject> targetIndicators = new List<GameObject>();           // The pool of target indicators.
-    private List<GameObject> arrowIndicators = new List<GameObject>();            // The pool of arrow indicators.
+    private List<GameObject> targetIndicators = new List<GameObject>();          // The pool of target indicators.
+    private List<GameObject> arrowIndicators = new List<GameObject>();           // The pool of arrow indicators.
     [SerializeField]
-    private int currentTargetIndicatorIndex = 0;                        // The first unassigned target indicator.
-    private int currentArrowIndicatorIndex = 0;                         // The first unassigned arrow indicator.
+    private int currentTargetIndicatorIndex = 0;                                 // The first unassigned target indicator.
+    private int currentArrowIndicatorIndex = 0;                                  // The first unassigned arrow indicator.
 
     /* Update is called once per frame after Update functions have been called. */
     private void LateUpdate()
@@ -22,7 +22,7 @@ public class IndicatorController : MonoBehaviour
         DrawIndicators();
     }
 
-    /**/
+    /* Handles drawing both the on-screen target and off-screen arrow indicators. */
     private void DrawIndicators()
     {
         ResetIndicatorPools();
@@ -35,9 +35,63 @@ public class IndicatorController : MonoBehaviour
                 screenPos.x > 0 && screenPos.x < Screen.width &&
                 screenPos.y > 0 && screenPos.y < Screen.height)
             {
-                // Get indicator and set location...
+                // The object is located on-screen.
                 GameObject targetIndicator = GetTargetIndicator();
                 targetIndicator.transform.position = screenPos;
+            }
+            else
+            {
+                // The object is loacted off-screen.
+
+                // Translate screen coordinates to make the screen centre the origin.
+                Vector3 screenOrigin = new Vector3(Screen.width, Screen.height, 0) * 0.5f;
+                screenPos -= screenOrigin;
+
+                // Find the angle from the origin to the mouse position.
+                float angle = Mathf.Atan2(screenPos.y, screenPos.x);
+                angle -= 90 * Mathf.Deg2Rad;
+                float angleSin = Mathf.Sin(angle);
+                float angleCos = Mathf.Cos(angle);
+
+                screenPos = screenOrigin + new Vector3(angleSin, angleCos, 0);
+
+                // Using the slope intercept (y = mx + b).
+                float gradient = angleCos / angleSin;
+
+                // Adds a small gap between the arrows and the edge fo the screen.
+                Vector3 screenBounds = screenOrigin * 0.9f;
+
+                // Determine the vertical direction for the arrow.
+                if (angleCos > 0)
+                {
+                    // The arrow is in the top half of the screen.
+                    screenPos = new Vector3(screenBounds.y / gradient, screenBounds.y, 0);
+                }
+                else
+                {
+                    // The arrow is in the bottom half of the screen.
+                    screenPos = new Vector3(-screenBounds.y / gradient, -screenBounds.y, 0);
+                }
+
+                // Determine the horizontal direction for the arrow.
+                if (screenPos.x > screenBounds.x)
+                {
+                    // The arrow is on the right side of the screen.
+                    screenPos = new Vector3(screenBounds.x, screenBounds.x * gradient, 0);
+                }
+                else if (screenPos.x < -screenBounds.x)
+                {
+                    // The arrow is on the left side of the screen.
+                    screenPos = new Vector3(-screenBounds.x, -screenBounds.x * gradient, 0);
+                }
+
+                // Remove the coordinate translation.
+                screenPos += screenOrigin;
+
+                // Set an arrow indicator.
+                GameObject arrowIndicator = GetArrowIndicator();
+                arrowIndicator.transform.position = screenPos;
+                arrowIndicator.transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
             }
         }
 
@@ -65,6 +119,27 @@ public class IndicatorController : MonoBehaviour
         return targetIndicator;
     }
 
+    /* Returns a arrow indicator UI GameObject from the arrow indicator pool. */
+    private GameObject GetArrowIndicator()
+    {
+        GameObject arrowIndicator;
+        if (currentArrowIndicatorIndex < arrowIndicators.Count)
+        {
+            // Use the existing arrow indicator.
+            arrowIndicator = arrowIndicators[currentArrowIndicatorIndex];
+        }
+        else
+        {
+            // Create a new arrow indicator.
+            arrowIndicator = Instantiate(arrowIndicatorPrefab);
+            arrowIndicator.transform.SetParent(transform);
+            arrowIndicators.Add(arrowIndicator);
+        }
+
+        currentArrowIndicatorIndex++;
+        return arrowIndicator;
+    }
+
     /* Resets the indicators to be reassigned from the first index. */
     private void ResetIndicatorPools()
     {
@@ -81,6 +156,14 @@ public class IndicatorController : MonoBehaviour
             GameObject targetIndicator = targetIndicators[targetIndicators.Count - 1];
             targetIndicators.Remove(targetIndicator);
             Destroy(targetIndicator);
+        }
+
+        // Arrow Indicators.
+        while (arrowIndicators.Count > currentArrowIndicatorIndex)
+        {
+            GameObject arrowIndicator = arrowIndicators[arrowIndicators.Count - 1];
+            arrowIndicators.Remove(arrowIndicator);
+            Destroy(arrowIndicator);
         }
     }
 }
